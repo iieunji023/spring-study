2828563b20440bee749a60892f4ded57784ec544
iieunji023 <iieunji023@gmail.com>
2024-08-15 15:50:19 +0900

    회원 서비스 로직 구현

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,53 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원X
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+
+        validateDuplicateMember(member);    // 중복회원 검증
+        memberRepository.save(member);  // 통과시 저장
+        return member.getId();
+
+    }
+
+    // 이렇게 자주 쓰는 메서드는 Extract method로 설정(Ctrl + Shift + m) => 일종의 리팩토링
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    System.out.println(m);
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /*
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+
+    }
+
+    public Optional<Member> findOne(Long id) {
+        return memberRepository.findById(id);
+
+    }
+
+}
